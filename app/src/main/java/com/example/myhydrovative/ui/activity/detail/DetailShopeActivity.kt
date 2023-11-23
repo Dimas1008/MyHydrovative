ffe93227d40d9afa073168569787bdb013283a10
Dimas Surya Wirastama <dimas.shie.jhoki@gmail.com>
2023-11-23 14:51:38 +0700

    Menambahkan Detail Activity Shope

---
+++ app/src/main/java/com/example/myhydrovative/ui/activity/detail/DetailShopeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myhydrovative.ui.activity.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myhydrovative.R
+
+class DetailShopeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_shope)
+    }
+}
